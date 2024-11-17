6a5bb4191a4fa3c5b7f2c869767f52a8caae5db1
Sasha <alecsand.unit@gmail.com>
2024-11-17 14:43:08 +0200

    dz

--- ConsoleApp17/Program.cs
+++ ConsoleApp17/Program.cs
@@ -7,7 +7,7 @@ class Employee
     public int Id { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
-    public int Age { get; set; }
+    public int Age { get; set; } 
     public int DepId { get; set; }
 }
 
